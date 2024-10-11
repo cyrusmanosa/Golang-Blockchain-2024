4d72edb63c354601c0c521167b81dbf86f6ecb1a
Cyrus Man <114724876+cyrusmanosa@users.noreply.github.com>
2024-10-11 12:20:40 +0900

    clean up

--- blockchain-web/blockchain-back/main.go
+++ blockchain-web/blockchain-back/main.go
@@ -9,11 +9,6 @@ import (
 	"runtime"
 )
 
-const (
-	pdfPath = "/Users/cyrusman/Desktop/ECCコンピューター専門学校/Year-3/Y3-Sem1/ITゼミ演習１/blockchain-web/blockchain-back/dsl/Original/履歴書.pdf"
-	svgPath = "/Users/cyrusman/Desktop/ECCコンピューター専門学校/Year-3/Y3-Sem1/ITゼミ演習１/blockchain-web/blockchain-back/dsl/Original/Svg/TestSVG.svg"
-)
-
 func main() {
 	if len(os.Args) < 2 {
 		runtime.Goexit()
@@ -24,7 +19,7 @@ func main() {
 		controllers.Cli()
 	case "server":
 		data := dsl.PngToSvg()
-		server.Gin(pdfPath, svgPath, data)
+		server.Gin(data)
 	default:
 		runtime.Goexit()
 	}
