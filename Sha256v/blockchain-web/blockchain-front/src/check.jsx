d5508fc29863e4263536041b2cd2a0c5577f3544
Cyrus Man <114724876+cyrusmanosa@users.noreply.github.com>
2024-11-08 13:42:08 +0900

    update folder

--- Sha256v/blockchain-web/blockchain-front/src/check.jsx
+++
@@ -1,7 +0,0 @@
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-
-export default function Check() {
-    const { name } = useParams();
-    axios.post(`http://localhost:8080/Check/${name}`,);
-}
---
+++ blockchain-back/Argon2v/controllers/cliForCommand.go
@@ -0,0 +1,34 @@
+package controllers
+
+import (
+	"Argon2/blockchain"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+func (cli *CommandLine) AddBlockForGuestForCommand(data string) {
+	cli.blockchain.AddBlockForDoc(data)
+	fmt.Println("Added Block!")
+}
+
+func (cli *CommandLine) CommandRun() {
+
+	printChainCmd := flag.NewFlagSet("print", flag.ExitOnError)
+	err := printChainCmd.Parse(os.Args[2:])
+	if err != nil {
+		log.Println("2 error: ", err)
+	}
+	if printChainCmd.Parsed() {
+		cli.PrintChain()
+	}
+}
+
+func Cli() {
+	chain := blockchain.InitBlockChainForDoc()
+	defer chain.Database.Close()
+
+	cli := CommandLine{chain}
+	cli.CommandRun()
+}
