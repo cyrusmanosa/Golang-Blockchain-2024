703e693ed328e8a4d24ababacb83b9fab5145fe3
Cyrus Man <114724876+cyrusmanosa@users.noreply.github.com>
2024-11-12 16:10:46 +0900

    update

--- Argon2v/blockchain-front/src/check.jsx
+++
@@ -1,7 +0,0 @@
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-
-export default function Check() {
-    const { name } = useParams();
-    axios.post(`http://localhost:8081/Check/${name}`,);
-}
